ebbd5bd4d41742dd54f3049201c683c2c248665a
Aykayabby <abigaileke2002@gmail.com>
2022-04-12 19:54:47 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+
+	void print_alphabet(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{	
+	_putchar(alphabet);
+}
+	_putchar('\n');
+}
