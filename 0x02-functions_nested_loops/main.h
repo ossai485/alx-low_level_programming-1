010067d32a59157b894a43a9cff5da83cf43c300
Aykayabby <abigaileke2002@gmail.com>
2022-04-12 19:43:40 -0700

    commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,13 @@
 #ifindef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
-int_putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
-int_abs(int n);
+int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
