396b9ffccaf85a68f2dd311b039160d3b7ce749a
Aykayabby <abigaileke2002@gmail.com>
2022-04-15 17:50:33 -0700

    commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,6 @@
-#indef MAIN_H_
-#define _MAIN_H_
 
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,6 +12,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
-
-#endif
+vid print_number(int n);
