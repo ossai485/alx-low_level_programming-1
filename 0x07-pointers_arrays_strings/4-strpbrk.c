fbb34b96a363150b579c18d91c45c10c1c001a3e
Aykayabby <abigaileke2002@gmail.com>
2022-04-27 20:28:22 -0700

    commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strstr - return to first char
- * @haystack: string to find substring
- * @needle: substring to find match
+ * _strpbrk - return to first char
+ * @s: string to find substring
+ * @accept: substring to find match
  * Return: pointer to first matching char
  */
 
@@ -12,13 +12,8 @@ char *_strpbrk(char *s, char *accept)
 	unsigned int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
-
 	for (j = 0; accept[j] != '\0'; j++)
-	
 		if (s[i] == accept[j])
-
 			goto exit;
-
 exit: return (s[i] != '\0' ? s + i : '\0');
-	
 }
