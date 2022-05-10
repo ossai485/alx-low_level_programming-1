449adb53ee4366b771f3e7224f135da036d49ff4
Aykayabby <abigaileke2002@gmail.com>
2022-05-09 19:54:27 -0700

    commit

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates array
+ * @min: first number
+ * @max: second number
+ *
+ * Return: pointer to newly allocated memory or NULL
+ */
+
+int *array_range(int min, int max);
+{
+	int i, l;
+	int *a;
+
+	if (min > max)
+		return (NULL);
+	l = max - min + 1;
+	a = malloc(sizeof(int) * l);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < l; i++, min++)
+	{
+		a[i] = min;
+	}
+	return (a);
+}
