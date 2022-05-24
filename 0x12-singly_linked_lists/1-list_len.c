42792d93dbb54c981a2fde3454a83f88dd4330e5
Aykayabby <abigaileke2002@gmail.com>
2022-05-24 12:17:26 -0700

    commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib>
+#include "main.h"
+
+/**
+ * list_len - returns number of elements
+ * @h: pointer to list
+ *
+ * return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
