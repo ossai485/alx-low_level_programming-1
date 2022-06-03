8681562f34e0919c5ecd0c66b49a30f721b90f83
Aykayabby <abigaileke2002@gmail.com>
2022-06-03 07:35:33 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - entry point
  *
- * return: always 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -13,5 +13,6 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	
 	return (0);
 }
