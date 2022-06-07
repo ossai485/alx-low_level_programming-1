10c63e908d185cdc48524cefc26995ab67dfc7af
Aykayabby <101524184+Aykayabby@users.noreply.github.com>
2022-06-07 07:09:51 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,37 +1,37 @@
-#include "main.h"
 /**
- * leet - encodes a string
- * @str: the string to encode
+ * leet - a function that encodes a string into 1337
  *
- * Return: the encode string
+ * @s: string input
+ *
+ * Return: @s
  */
 
-char *leet(char *str)
+char *leet(char *s)
 {
-	int i, j;
-	char c[] = "aAeEoOtTIL";
-	char n[] = "4433007711";
+	int i, c = 0;
+	int sl[] = {97, 101, 111, 116, 108};
+	int ul[] = {65, 69, 79, 84, 76};
+	int n[] = {52, 51, 48, 55, 49};
 
-	i = 0;
-
-	while (str[i] != '\0')
-
+	/*iterate values in array s*/
+	while (s[c] != '\0')
 	{
-		j = 0;
-
-		while (c[j] != '\0')
+		/**
+		 * loop through array value five times
+		 * to check if the value is equal to
+		 * any small letter in array sl or
+		 * upper letter in array ul and if it
+		 * is replace it with the value in array n
+		 */
+		for (i = 0; i < 5; i++)
 		{
-
-		if (str[i] == c[j])
-
-		{
-			str[i] = n[j];
+			if (s[c] == sl[i] || s[c] == ul[i])
+			{
+				s[c] = n[i];
+				break;
+			}
 		}
-
-		j++;
+		c++;
 	}
-
-	i++;
-}
-return (str);
+	return (s);
 }
