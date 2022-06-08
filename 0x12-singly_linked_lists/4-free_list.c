c10f58e6e51d407f4d87faa3ffa2f5c460f59a81
Aykayabby <101524184+Aykayabby@users.noreply.github.com>
2022-06-08 14:12:49 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,10 @@
+#include "lists.h"
 #include <stdlib.h>
-#inlude "lists.h"
 
 /**
- * free_list - frees a linked list
- * @head: A pointer to the list_tlist
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the list_t list.
  */
-
 void free_list(list_t *head)
 {
 	list_t *tmp;
